8ce2aa0088a197885204c436de9ba1679bfcaee6
Christiana Otoboh <christianaotoboh@gmail.com>
2023-10-07 18:20:49 +0100

    Added second question

--- Assignment.js
+++ Assignment.js
@@ -1,7 +1,14 @@
 // ++Question Write a Javascript code to print the unique elements in 
 // [1,1,1,2,2,3,2,2,4,9,3,2,6,8,4,7,0,1,0,5,0] What is the length if that array?
 
+// No 1
 const arr = [1,1,1,2,2,3,2,2,4,9,3,2,6,8,4,7,0,1,0,5,0];
 new Set(arr);
 console.log(new Set(arr));
+
+
+
+// No 2
+const arrlist = [1,1,1,2,2,3,2,2,4,9,3,2,6,8,4,7,0,1,0,5,0];
+let length = console.log(arrlist.length);
 
